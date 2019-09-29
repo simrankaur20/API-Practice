1eac1ed78010069031fcda68773a3a992bd8cce4
unknown <simrankr361@gmail.com>
2019-09-29 16:11:49 +0530

    second commit

--- index.js
+++ index.js
@@ -5,6 +5,10 @@
 //Json data can easily be nested
 //XML is also a format, like JSON, can also be used (more longer and a bit difficult to use)
 
+
+//JSON.stringify() - Javascript object to JSON
+//JSON.parse() - JSON to Javascript object
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
@@ -17,13 +21,26 @@ app.get("/" , function(req,res){
 });
 
 app.post("/" , function(req,res){
-    console.log(req.body.crypto);
-})
+    //    console.log(req.body.crypto);
+    var crypto = req.body.crypto;
+    var fiat = req.body.fiat;
+    // storing values that user chose
 
-request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD" , function(error,message,body) {
-    console.log(body);
-})
+    //we wish to generate request to api as per the values chosen by server
 
+    var baseURL = "https://apiv2.bitcoinaverage.com/indices/local/ticker/" ;
+
+    var finalURL = baseURL + crypto + fiat;
+
+    request(finalURL , function(error,response,body) {
+        // hum api se request karenge to resonse milega in body in JSON form , convert it into Javascript object
+
+        var data = JSON.parse(body);
+        var price = data.last;
+        
+        res.send("<h1> The current price of " + crypto + " is : " + price + fiat + "</h1>" );
+    });
+})
 app.listen(3000 , function(){
     console.log("server is running on port 3000");
 });
